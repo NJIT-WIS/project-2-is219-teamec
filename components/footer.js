6f28d6d1a2fcc5f7707d12602b6df5bd8f919e37
Charles Wicklund <chwicklund@gmail.com>
2023-04-18 21:17:23 -0400

    Edited footer

--- components/footer.js
+++ components/footer.js
@@ -7,15 +7,57 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <p>&copy; 2023 TeamEC, All rights reserved.</p>
-        <ul className={styles.footerLinks}>
-          <li>
-            <a href="#">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-        </ul>
+          <table>
+              <tr>
+                  <td><a href="#">Home</a></td>
+                  <td><a href="#">Privacy Policy</a></td>
+              </tr>
+        </table>
       </div>
     </footer>
   )
 }
+
+//<tr>
+//             <td>
+//               <ul className={styles.footerLinks}>
+//                 <li>
+//                   <a href="#">Home</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">Blog</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">Resources</a>
+//                 </li>
+//               </ul>
+//             </td>
+//             <td>
+//               <ul className={styles.footerLinks}>
+//                 <li>
+//                   <a href="#">Privacy Policy</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">Contact Us</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">About</a>
+//                 </li>
+//               </ul>
+//             </td>
+//           </tr>
+
+//<table>
+//               <tr>
+//                   <td><a href="#">Home</a></td>
+//                   <td><a href="#">Privacy Policy</a></td>
+//               </tr>
+//               <tr>
+//                   <td><a href="#">Blog</a></td>
+//                   <td><a href="#">Contact Us</a></td>
+//               </tr>
+//               <tr>
+//                   <td><a href="#">Resources</a></td>
+//                   <td><a href="#">About</a></td>
+//               </tr>
+//         </table>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import Footer from "../components/footer";
 
 export default function Home({ allPostsData }) {
   return (
@@ -32,6 +33,7 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+        <Footer/>
     </Layout>
   )
 }
