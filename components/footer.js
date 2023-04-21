86a6141dc12754d45a52ff49d911b86ee8c311db
Charles Wicklund <chwicklund@gmail.com>
2023-04-21 13:40:53 -0400

    Edited footer

--- components/footer.js
+++ components/footer.js
@@ -7,12 +7,23 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <p>&copy; 2023 TeamEC, All rights reserved.</p>
-          <table>
-              <tr>
-                  <td><a href="#">Home</a></td>
-                  <td><a href="#">Privacy Policy</a></td>
-              </tr>
-        </table>
+        <table>
+               <tr>
+                   <td><a href="#">Home</a></td>
+                   <td>ㅤㅤ</td>
+                   <td><a href="#">Privacy Policy</a></td>
+               </tr>
+               <tr>
+                   <td><a href="#">Blog</a></td>
+                   <td>ㅤㅤ</td>
+                   <td><a href="#">Contact Us</a></td>
+               </tr>
+               <tr>
+                   <td><a href="#">Resources</a></td>
+                   <td>ㅤㅤ</td>
+                   <td><a href="#">About</a></td>
+               </tr>
+         </table>
       </div>
     </footer>
   )
@@ -46,6 +57,37 @@ export default function Footer() {
 //               </ul>
 //             </td>
 //           </tr>
+
+//<table>
+//               <tr>
+//                   <td>
+//                   <ul className={styles.footerLinks}>
+//                         <li>
+//                             <a href="../pages/Home.js">Home</a>
+//                         </li>
+//                         <li>
+//                             <a href="#">Blog</a>
+//                         </li>
+//                         <li>
+//                             <a href="#">Resources</a>
+//                         </li>
+//                     </ul>
+//                   </td>
+//                   <td>
+//                     <ul className={styles.footerLinks}>
+//                         <li>
+//                             <a href="#">Privacy Policy</a>
+//                         </li>
+//                         <li>
+//                             <a href="#">Contact Us</a>
+//                         </li>
+//                         <li>
+//                             <a href="#">About</a>
+//                         </li>
+//                     </ul>
+//                   </td>
+//               </tr>
+//         </table>
 
 //<table>
 //               <tr>
