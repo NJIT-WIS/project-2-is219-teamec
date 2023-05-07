692b61ecfd022de65e5b4cf00eff435dcdc0e3e1
Emmanuel Chiobi <ec362@njit.edu>
2023-05-07 14:29:58 -0400

    Added some more stuff

---
+++ pages/Contact.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Layout, { siteTitle } from '../components/layout';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit form data
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+      </label>
+      <label>
+        Message:
+        <textarea name="message" value={formData.message} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
