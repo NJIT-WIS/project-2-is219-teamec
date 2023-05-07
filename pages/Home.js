bf25b3979adfb0485b261bc8249f59936c7d35e2
ESinson <70695462+EDS435@users.noreply.github.com>
2023-05-07 18:34:10 -0400

    Hero images added

--- pages/Home.js
+++ pages/Home.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import { getSortedPostsData } from '../lib/posts'
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 
 export default function Home() {
@@ -16,10 +17,15 @@ export default function Home() {
             <Navbar/>
         </section>
       <main className={styles.container}>
-        <h1>Welcome to MyWebClass</h1>
-        <p>Our mission is to provide high-quality, accessible web development education to students around the world. Whether you're a complete beginner or an experienced developer looking to expand your skills, we have a course for you.</p>
-        <button>Learn How to Get Involved</button>
+          <div className="hero-image">
+              <div className="hero-text">
+                  <h1>MyWebClass</h1>
+                  <p>MyWebClass offers online courses in web development, including HTML, CSS, JavaScript, React, and more. Enroll today and start your journey to becoming a web developer!</p>
+                  <button>Learn How to Get Involved</button>
+              </div>
+          </div>
       </main>
+         <Footer/>
     </>
   );
 }
