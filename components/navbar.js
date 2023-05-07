4cbd05da5ef90539517e72cbd64ba572c3ae336b
Charles Wicklund <chwicklund@gmail.com>
2023-05-07 15:47:13 -0400

    Added navbar component

---
+++ components/navbar.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <p>Blog</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/resources">
+            <p>Resources</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/services">
+            <p>Services</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <p>About</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <p>Contact</p>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
