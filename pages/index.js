4fc06b93b9fd87a37d8c3ff2517101d8af32f0c0
Charles Wicklund <chwicklund@gmail.com>
2023-05-07 22:03:40 -0400

    Removed blog call in 2 pages

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,6 @@ export default function Home({ allPostsData }) {
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
             </p>
         </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
         <Footer/>
     </Layout>
   )
