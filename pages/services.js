5268a84cc776aa2a0c21fff5c08f82c01534ca82
ESinson <70695462+EDS435@users.noreply.github.com>
2023-05-07 22:41:01 -0400

    Hero and CTA set up for services

--- pages/services.js
+++ pages/services.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import Navbar from "../components/navbar";
+import Image from "next/image";
+import images from '../public/images/index'
+import styles from '../styles/Home.module.css';
+import Footer from "../components/footer";
 
 export default function Home({ allPostsData }) {
   return (
@@ -19,7 +23,15 @@ export default function Home({ allPostsData }) {
 
         <div className="flex flex-col md:flex-row justify-center items-center mb-16">
           <div className="w-full md:w-1/2 md:pr-8">
-            <img src="/professional-development.jpg" alt="Professional Development" className="w-full h-auto mb-8" />
+            <Image
+          priority
+          src={images.pd}
+          width={500}
+          height={400}
+          objectPosition="center"
+          layout = "responsive"
+          alt="pd example"
+        />
 
             <h2 className="text-2xl font-bold mb-4">Professional Development</h2>
 
@@ -27,20 +39,35 @@ export default function Home({ allPostsData }) {
               We offer a wide range of professional development opportunities for educators and school administrators. Our workshops, seminars, and courses are designed to enhance skills and knowledge in various areas, including:
             </p>
 
-            <ul className="list-disc pl-8 mb-8">
-              <li>Curriculum design and development</li>
-              <li>Assessment and evaluation</li>
-              <li>Instructional strategies</li>
-              <li>Technology integration</li>
-              <li>Leadership development</li>
-            </ul>
 
-            <a href="/professional-development" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ease-in-out">Learn more</a>
+              <h3>Curriculum design and development</h3>
+              <p>This workshop involves the creation and improvement of educational programs or courses that align with specific learning objectives and meet the needs of learners. Participants in a Professional Development service focused on curriculum design and development may learn how to design and develop effective educational programs, including how to create clear learning outcomes, design assessments, and plan learning activities.</p>
+
+              <h3>Assessment and evaluation</h3>
+              <p> This workshop involves the systematic collection and analysis of data to measure the effectiveness of educational programs and inform decision-making. Participants in a Professional Development service focused on assessment and evaluation may learn how to design and use different types of assessments, interpret assessment results, and use data to inform instructional decisions and improve student learning outcomes.</p>
+
+              <h3>Instructional strategies</h3>
+              <p>This workshop involves the use of effective teaching methods and approaches to support student learning and engagement. Participants in a Professional Development service focused on instructional strategies may learn about evidence-based practices, active learning techniques, and differentiated instruction to support a variety of learners.</p>
+
+              <h3>Technology integration</h3>
+              <p>This workshop involves the intentional use of technology to enhance teaching and learning. Participants in a Professional Development service focused on technology integration may learn how to use different types of technology tools and platforms to engage learners, create interactive learning experiences, and support online or hybrid learning environments.</p>
+
+              <h3>Leadership development</h3>
+              <p>This workshop involves the development of skills and strategies to lead and support educational programs or organizations effectively. Participants in a Professional Development service focused on leadership development may learn about different leadership styles, strategies for effective communication, and how to build strong teams and cultures of learning.</p>
+
           </div>
 
-          <div className="w-full md:w-1/2 md:pl-8">
-            <img src="/customized-training.jpg" alt="Customized Training and Support" className="w-full h-auto mb-8" />
+            <Image
+          priority
+          src={images.cts}
+          width={500}
+          height={400}
+          objectPosition="center"
+          layout = "responsive"
+          alt="pd example"
+        />
 
+          <div className="w-full md:w-1/2 md:pl-8">
             <h2 className="text-2xl font-bold mb-4">Customized Training and Support</h2>
 
             <p className="text-lg mb-8">
@@ -51,6 +78,7 @@ export default function Home({ allPostsData }) {
           </div>
         </div>
       </div>
+      <Footer/>
     </Layout>
   )
 }
